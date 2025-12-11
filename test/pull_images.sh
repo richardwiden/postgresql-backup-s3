38294b440dfeced7d4f3ec38908cc95589d82f38
Richard Widén <richardwiden@gmail.com>
2025-12-12 00:17:22 +0100

    Change to custom minion image since they have stopped supporting official images. Start defaulting to us-east-1

--- test/pull_images.sh
+++ test/pull_images.sh
@@ -2,5 +2,5 @@
 docker pull -q amazon/aws-cli
 docker pull -q ${POSTGRES_BASE_IMAGE}
 docker pull -q ${POSTGRES_CLIENT_IMAGE}
-docker pull -q bitnami/minio:latest
+docker pull -q minio/minio:latest
 docker build docker -t ${POSTGRES_BACKUP_IMAGE} --build-arg POSTGRES_BASE_IMAGE
--- test/setup_minio.sh
+++ test/setup_minio.sh
@@ -4,8 +4,9 @@ set -e
 docker run --rm --network "${TEST_NETWORK}" --name "${S3_HOST}" -d -p 9000 \
   -e MINIO_ROOT_USER="$S3_ACCESS_KEY_ID" \
   -e MINIO_ROOT_PASSWORD="$S3_SECRET_ACCESS_KEY" \
+  -e MINIO_REGION="us-east-1" \
   --health-cmd "curl -I http://localhost:9000/minio/health/live" --health-start-period 5s --health-interval 1s --health-timeout 1s --health-retries 10 \
-  bitnami/minio:latest
+  firstfinger/minio:latest-amd64
 sleep 1
 while [ "$health_status" != "healthy" ]; do
     health_status=$(docker inspect --format='{{.State.Health.Status}}' "${S3_HOST}")
@@ -17,4 +18,5 @@ docker run --rm -i --network "${TEST_NETWORK}" --name "${AWS_CLI_HOST}" \
   -e AWS_ACCESS_KEY_ID="${S3_ACCESS_KEY_ID}" \
   -e AWS_SECRET_ACCESS_KEY="${S3_SECRET_ACCESS_KEY}" \
   -e AWS_EC2_METADATA_DISABLED=true \
+  -e AWS_REGION="us-east-1" \
   amazon/aws-cli --endpoint-url "${S3_ENDPOINT}" s3 mb "s3://${S3_BUCKET}"
